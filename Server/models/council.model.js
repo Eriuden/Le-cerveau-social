9f029200f70de0e68cd7fb3b27eb05a02f1f64a6
Eriuden <antoinecarrette@hotmail.fr>
2025-01-14 10:11:54 +0100

    build: councilModel first commit on 14/01/2025

---
+++ Server/models/council.model.js
@@ -0,0 +1,54 @@
+//Lui va être plus complexe
+//Il faut en plus de l'id générée auto, un nom, un thème, et un président
+//Il faut pouvoir y englober des users et des posts, tout en rendant l'accés privé
+
+
+const mongoose = require('mongoose');
+
+const CouncilSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        } ,
+        theme:{
+            type: String,
+            required: true
+        },
+        presidentId:{
+            type: String,
+            required: true
+        },
+
+        posts: {
+            type: [
+                {
+                    posterId: String,
+                    posterName: String,
+                    text: String,
+                    video:String,
+                    comments: {
+                        type: [
+                            {
+                            commenterId: String,
+                            commenterName: String,
+                            text: String,
+                            video: String,
+                            timeStamp: Number
+                            }
+                        ],
+                        required: true, 
+                    },
+                    timestamp: Number,
+                }
+            ],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const CouncilModel = mongoose.model('council',CouncilSchema)
+module.exports = CouncilModel
