be15696a9714b8ad7433d1883c50bd0f8451930a
Eriuden <antoinecarrette@hotmail.fr>
2025-01-14 09:21:04 +0100

    build: server files architecture

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 /*ressource nécessaire
     des comptes users, mais également des comptes modos 
     des posts (aavec les fameuses réactions entre autres)
-    des groupes de discussions (thèmes à trouver, comme art, philo, sport, etc...)
+    des groupes de discussions, appelons les conseils (thèmes à trouver, comme art, philo, sport, etc...)
     un système de modo par IA
 */
