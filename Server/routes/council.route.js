8853c9a801c0ceaf2357a5d68d4888a3eb93d5cd
Eriuden <antoinecarrette@hotmail.fr>
2025-01-16 09:52:49 +0100

    build: councilRoute

---
+++ Server/routes/council.route.js
@@ -0,0 +1,12 @@
+const router = require("express").Router()
+const councilController = require("../controllers/council.controller")
+const uploadController = require("../controllers/upload.controller")
+const multer = require ("multer")
+const upload = multer()
+
+router.get("/", councilController.getAllCouncils)
+router.get("/:id", councilController.getACouncil)
+router.put("/:id", councilController.updateCouncil)
+router.delete("/:id", councilController.deleteCouncil)
+router.post("/", upload.single("file", councilController.createArticle))
+router.post("/upload-councilBanner", upload.single("file"), uploadController.uploadCouncilBanner)
