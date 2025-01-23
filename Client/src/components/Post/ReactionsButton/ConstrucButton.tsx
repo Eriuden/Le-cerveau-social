cc8b0e89c53275f7ef387949d0d1c13321f3a49c
Eriuden <antoinecarrette@hotmail.fr>
2025-01-23 19:29:11 +0100

    build: constructButton 23/01/2025

---
+++ Client/src/components/Post/ReactionsButton/ConstrucButton.tsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from "react"
+import { UidContext } from "../../AppContext"
+import { findPostConstructive, constructiveAnymore} from "../../../redux/actions/post.action"
+import { useDispatch } from "react-redux"
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+
+export const ConstrucButton = (postProps:any) => {
+    const [constructive, setConstructive] = useState(false)
+    const uid = useContext(UidContext)
+    const dispatch = useDispatch()
+
+    const findConstructive = () => {
+        dispatch(findPostConstructive(postProps._id, uid))
+        setConstructive(true)
+    }
+    const findConstructiveAnymore = () => {
+        dispatch(constructiveAnymore(postProps._id, uid))
+        setConstructive(false)
+    }
+
+    useEffect(() => {
+        if (postProps.interesting.includes(uid)) setConstructive(true)
+        else setConstructive(false)
+    }, [uid, postProps.constructive, constructive])
+
+  return (
+    <div>
+        {
+        uid === null && (
+            <Popup trigger= {<img src="./img/icons/heart.svg" alt="like" />} position= {
+                ['bottom center', 'bottom right', 'bottom left']} closeOnDocumentClick >
+                    <div>Vous devez être connecté pour liker un post</div>
+            </Popup> 
+        )}
+        {
+        uid && constructive === false && (
+            <img src="./img/icons/heart.svg" onClick={findConstructive} alt="like"/>
+        )}
+
+        {
+        uid && constructive && (
+            <img src="./img/icons/heart-filled.svg" onClick={findConstructiveAnymore} alt="unLike" />
+        )}
+        <span>{postProps.constructive.length}</span>
+    </div>
+  )
+}
