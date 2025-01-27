25f74b7ae0a306f887b75d27762461ac1a567872
Eriuden <antoinecarrette@hotmail.fr>
2025-01-27 09:37:27 +0100

    build: Thread 27/01/2025

---
+++ Client/src/components/Post/PostCard.tsx
@@ -0,0 +1,5 @@
+export const PostCard = ({postCardProps}) => {
+  return (
+    <div>PostCard</div>
+  )
+}
---
+++ Client/src/components/Thread.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+import { useEffect, useState } from "react";
+import { getPosts } from "../redux/actions/post.action";
+import { useDispatch, useSelector } from "react-redux";
+import {PostCard} from "../components/Post/PostCard"
+import { isEmpty } from "./Utils";
+
+export default function Thread() {
+  const [loadPost, setLoadPost] = useState(true);
+  const [count, setCount] = useState(5);
+  const dispatch = useDispatch();
+  const posts = useSelector((state:any) => state.postReducer);
+
+  const loadMore = () => {
+    //première partie: notre position actuelle
+    //seconde: la taille actuelle du contenu
+    //donc si notre position dépasse la taille du contenue
+    //on charge la suite de ce dernier
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.scrollingElement.scrollHeight
+    ) {
+      setLoadPost(true);
+    }
+  };
+
+  useEffect(() => {
+    //si loadpost est true, donc, si le contenu est chargé
+    //on prend cinq posts (cont fait ici 5)
+    //on demande de ne plus charger, du moins pour l'instant
+    // mais on rajoute 5 à count pour laisser en place les anciens
+    if (loadPost) {
+      dispatch(getPosts(count));
+      setLoadPost(false);
+      setCount((prevCount) => setCount(prevCount + 5));
+    }
+
+    window.addEventListener("scroll", loadMore);
+    return () => window.removeEventListener("scroll", loadMore);
+  }, [loadPost, dispatch, count]);
+
+  return (
+    <div className="thread-container">
+      <ul>
+        {!isEmpty(posts[0]) &&
+          posts.map((post:any) => {
+            return <PostCard postCardProps={post} key={post._id} />;
+          })}
+      </ul>
+    </div>
+  );
+}
