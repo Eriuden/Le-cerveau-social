7e143aebd6f09693c5a738275646a03cbf8bc340
Eriuden <antoinecarrette@hotmail.fr>
2025-01-29 16:22:17 +0100

    commit: DeletePost 29/01/2025

---
+++ Client/src/components/Post/DeletePostCard.tsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../redux/actions/post.action'
+
+type propsType = {
+    _id: string,
+    postId: string,
+    commenterId: string,
+    text: string
+  }
+
+
+export default function DeletePostCard(props:propsType) {
+  const dispatch = useDispatch()
+  const deleteQuote = () => dispatch(deletePost(props.id))
+  return (
+    <div 
+      onClick={() => {
+        if (window.confirm('Voulez vous supprimer cet article ?')){
+          deleteQuote()
+        }
+      }}
+    >
+      <img src="./img/icons/trash.svg" alt="" />
+      
+    </div>
+  )
+}
