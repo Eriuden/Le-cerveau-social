888e2309a75b7655ff1d87c7aaf7d70b7b877165
Eriuden <antoinecarrette@hotmail.fr>
2025-02-09 16:32:59 +0100

    build: Home 09/02/2025

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,9 +1,35 @@
-export const Home = () => {
+import { UidContext } from "../components/AppContext";
+import NewPostForm from "../components/Post/NewPostForm";
+import Thread from "../components/Thread";
+import Trends from "../components/Trend";
+import FriendHint from "../components/Profile/FriendHint";
+import { useContext } from "react";
+
+export default function Home() {
+  const uid = useContext(UidContext);
   return (
-    <div>
-      
+    <div className="home">
+      <div className="main">
+        <div className="home-header">
+          {uid ? ( 
+            <NewPostForm />
+          ) : (
+            "" 
+          )}
+        </div>
+        <Thread />
+      </div>
+      <div className="right-side">
+        <div className="right-side-container">
+          <div className="wrapper">
+            <Trends />
+            {uid && <FriendHint />}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
 
 
