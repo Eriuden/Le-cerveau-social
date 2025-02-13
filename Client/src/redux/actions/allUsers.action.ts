11513f556c6000597393d3996bec6f7a7605fd80
Eriuden <antoinecarrette@hotmail.fr>
2025-02-13 18:59:37 +0100

    fix: all users action 13/02/2025

--- Client/src/redux/actions/allUsers.action.ts
+++ Client/src/redux/actions/allUsers.action.ts
@@ -2,13 +2,11 @@ import axios from "axios"
 
 export const GET_ALL_USERS = "GET_ALL_USERS"
 
-export const getAllUsers = () => {
-    return (dispatch:any)=> {
+export const getAllUsers = (dispatch:any) => {   
         return axios
             .get(`${process.env.REACT_APP_API_URL}api/user`)
             .then((res)=> {
                 dispatch({type: GET_ALL_USERS, payload: res.data})
             })
             .catch((err) => window.alert(err))
-    }
 }
